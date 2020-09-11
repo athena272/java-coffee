3f1a54bcabfd6721cac9e4729cc029a466fd780c
athena272 <guilherme.alves073@academico.ifs.edu.br>
2020-09-11 01:26:35 -0300

    Separando Java_Basico do Java_POO

---
+++ Java_Basico/Prova_basico/ProvaQ02/ProvaQ02/src/provaq02/ProvaQ02.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provaq02;
+
+/**
+ *
+ * @author guilh
+ */
+public class ProvaQ02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double a = Math.pow(8, 2);
+        System.out.println(a);
+    }
+    
+}
---
+++ Java_Basico/Prova_basico/ProvaQ04/ProvaQ04/src/provaq04/ProvaQ04.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provaq04;
+
+/**
+ *
+ * @author guilh
+ */
+public class ProvaQ04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 1; i < 5; i++) {
+            if (i == 2) continue;
+            //Você também pode abrir chaves, e fazer normal
+            System.out.print("i = " + i + " ");
+        }
+    }
+    
+}
