ce926520b6985a68f06e6fb8d5d280e2e9ec4e31
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-10 17:41:22 -0300

    Create Livro.java

---
+++ Estudos/Alura/Livraria/src/Livro.java
@@ -0,0 +1,44 @@
+public class Livro {
+    String nome;
+    String descricao;
+    double valor;
+    String isbn;
+
+    //My Methods
+    void mostrarDetalhes() {
+        
+    }
+
+    //Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
