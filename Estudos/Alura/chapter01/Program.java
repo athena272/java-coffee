3c25fc5e23a98d9abb5bf51596057d6ef8c0ba01
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-10 17:40:57 -0300

    Update Program.java

--- Estudos/Alura/chapter01/Program.java
+++ Estudos/Alura/chapter01/Program.java
@@ -1,5 +1,6 @@
 class Program {
   public static void main(String[] args) {
-    System.out.println("O primeiro de muitos");
+    System.out.println("O primeiro de muitos codigos escritos em " + args[0] + " por " + args[1]);
   }
 }
+
