0aaa53473b1a3006406ff483002e2d089b4db9ad
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-10 18:25:15 -0300

    Create Autor.java

---
+++ Estudos/Alura/Livraria/src/Autor.java
@@ -0,0 +1,37 @@
+public class Autor {
+    String nome;
+    String email;
+    String cpf;
+
+    void mostrarDetalhes() {
+        System.out.println("Monstrando detalhes do Autor");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Email: " + this.email);
+        System.out.println("CPF: " + this.cpf);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+}
