59097bca2bfda15bd8660a4991b5c75d09d033de
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-10 17:41:19 -0300

    Create CalculadoraDeEstoque.java

---
+++ Estudos/Alura/Livraria/src/CalculadoraDeEstoque.java
@@ -0,0 +1,12 @@
+public class CalculadoraDeEstoque {
+    public static void main(String[] args) {
+        double livroJava8 = 60;
+        double livroTDD = 60;
+
+        int numberInt = (int) livroJava8;
+
+        double soma = livroTDD + livroJava8;
+        System.out.println(soma);
+        System.out.println(numberInt);
+    }
+}
