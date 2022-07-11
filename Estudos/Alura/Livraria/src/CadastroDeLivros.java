f2f355bd9bd76900333ee61a58002aad9b19b54c
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-11 04:46:12 -0300

    Update CadastroDeLivros.java

--- Estudos/Alura/Livraria/src/CadastroDeLivros.java
+++ Estudos/Alura/Livraria/src/CadastroDeLivros.java
@@ -5,12 +5,11 @@ public class CadastroDeLivros {
         autor.setEmail("rodrigo@gmail.com");
         autor.setCpf("XXX.XXX.XXX.XX");
 
-        Livro livro = new Livro();
+        Livro livro = new Livro(autor);
         livro.setNome("Java 8 Prático");
         livro.setDescricao("Novos recursos da linguagem");
         livro.setValor(59.90);
         livro.setIsbn("978-85-66250-46-6");
-        livro.setAutor(autor);
 
         livro.mostrarDetalhes();
 
@@ -19,12 +18,11 @@ public class CadastroDeLivros {
         outroAutor.setEmail("paulo@gmail.com");
         outroAutor.setCpf("YYY.YYY.YYY.YY");
 
-        Livro outroLivro = new Livro();
+        Livro outroLivro = new Livro(outroAutor);
         outroLivro.setNome("Lógica de Programação");
         outroLivro.setDescricao("Crie seus primeiros programas");
         outroLivro.setValor(59.90);
         outroLivro.setIsbn("978-85-66250-22-0");
-        outroLivro.setAutor(outroAutor);
 
         outroLivro.mostrarDetalhes();
 
