d234e8f7a97f4437ea9c324ef788de3eaac8f91a
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-11 09:25:15 -0300

    Update RegistroDeVendas.java

--- Estudos/Alura/Livraria/src/RegistroDeVendas.java
+++ Estudos/Alura/Livraria/src/RegistroDeVendas.java
@@ -1,4 +1,16 @@
 public class RegistroDeVendas {
-    Autor autor = new Autor();
+    public static void main(String[] args) {
+        Autor autor = new Autor();
+        autor.setNome("Mauricio Aniche");
 
+        LivroFisico livroFisico = new LivroFisico(autor);
+        livroFisico.setNome("Test-Driven Development");
+
+        Ebook ebook = new Ebook(autor);
+        ebook.setNome("Test-Driven Development");
+
+        CarrinhoDeCompra carrinhoDeCompra = new CarrinhoDeCompra();
+        carrinhoDeCompra.adiciona(livroFisico);
+        carrinhoDeCompra.adiciona(ebook);
+    }
 }
