ea3a5f7f1f6464ba371241813ca010b8fe66d950
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-11 10:32:46 -0300

    Update CarrinhoDeCompra.java

--- Estudos/Alura/Livraria/src/CarrinhoDeCompra.java
+++ Estudos/Alura/Livraria/src/CarrinhoDeCompra.java
@@ -1,5 +1,12 @@
 public class CarrinhoDeCompra {
+    private double total;
     public void adiciona(Livro livro) {
         System.out.println("Adicionando: " + livro);
+        livro.aplicaDescontoDe(0.16);
+        total += livro.getValor();
+    }
+
+    public double getTotal() {
+        return total;
     }
 }
