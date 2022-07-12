a22e026db72ba00a56cb7c86ce2c25377bf0d73a
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-11 21:06:42 -0300

    Create MiniLivro.java

---
+++ Estudos/Alura/Livraria/src/MiniLivro.java
@@ -0,0 +1,9 @@
+public class MiniLivro extends Livro{
+    public MiniLivro(Autor autor) {
+        super(autor);
+    }
+    @Override
+    public boolean aplicaDescontoDe(double porcentagem) {
+        return false;
+    }
+}
