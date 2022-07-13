467892ae2872de175074c5a9f81984b9cfb4c5bf
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-13 13:54:00 -0300

    Update Promocional.java

--- Estudos/Alura/Livraria/src/Promocional.java
+++ Estudos/Alura/Livraria/src/Promocional.java
@@ -1,3 +1,7 @@
 public interface Promocional {
     boolean aplicaDescontoDe(double porcentagem);
+
+    default boolean aplicaDescontoDe10Porcento(double porcentagem) {
+        return aplicaDescontoDe(0.1);
+    }
 }
