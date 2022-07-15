fd085207c901ba978d97a8911357550634095f05
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-15 14:30:52 -0300

    Update CarrinhoDeCompra.java

--- Estudos/Alura/Livraria/src/br/com/casadocodigo/livraria/CarrinhoDeCompra.java
+++ Estudos/Alura/Livraria/src/br/com/casadocodigo/livraria/CarrinhoDeCompra.java
@@ -13,6 +13,26 @@ public class CarrinhoDeCompra {
         this.total += produto.getValor();
     }
 
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Produto[] getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(Produto[] produtos) {
+        this.produtos = produtos;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+
     public double getTotal() {
         return total;
     }
