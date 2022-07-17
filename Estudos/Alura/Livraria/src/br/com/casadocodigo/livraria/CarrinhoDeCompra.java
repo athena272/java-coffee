3665010642dcf012e6187b7717a4bbaa50cb0a1a
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-17 14:48:01 -0300

    Update CarrinhoDeCompra.java

--- Estudos/Alura/Livraria/src/br/com/casadocodigo/livraria/CarrinhoDeCompra.java
+++ Estudos/Alura/Livraria/src/br/com/casadocodigo/livraria/CarrinhoDeCompra.java
@@ -3,12 +3,13 @@ package br.com.casadocodigo.livraria;
 import br.com.casadocodigo.livraria.produtos.Produto;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CarrinhoDeCompra {
     private double total;
     private List<Produto> produtos;
     public CarrinhoDeCompra() {
-        this.produtos = new List<Produto>();
+        this.produtos = new ArrayList<Produto>();
     }
     public void adiciona(Produto produto) {
         this.produtos.add(produto);
