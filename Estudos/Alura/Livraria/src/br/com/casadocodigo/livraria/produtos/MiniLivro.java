d1e85b5582e8b82b90c693f3d175044020e9c0c2
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-17 14:48:07 -0300

    Update MiniLivro.java

--- Estudos/Alura/Livraria/src/br/com/casadocodigo/livraria/produtos/MiniLivro.java
+++ Estudos/Alura/Livraria/src/br/com/casadocodigo/livraria/produtos/MiniLivro.java
@@ -6,4 +6,14 @@ public class MiniLivro extends Livro {
     public MiniLivro(Autor autor) {
         super(autor);
     }
+    @Override
+    public int compareTo(Produto outro) {
+        if(this.getValor() < outro.getValor()) {
+            return -1;
+        }
+        if(this.getValor() > outro.getValor()) {
+            return 1;
+        }
+        return 0;
+    }
 }
