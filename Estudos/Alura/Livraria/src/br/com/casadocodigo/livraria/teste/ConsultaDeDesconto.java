1fda7dde3013075412ff8f40818152100c6100b8
Guilherme Rosário Alves <guilhermera272@gmail.com>
2022-07-20 10:03:55 -0300

    Update ConsultaDeDesconto.java

--- Estudos/Alura/Livraria/src/br/com/casadocodigo/livraria/teste/ConsultaDeDesconto.java
+++ Estudos/Alura/Livraria/src/br/com/casadocodigo/livraria/teste/ConsultaDeDesconto.java
@@ -5,9 +5,11 @@ import br.com.casadocodigo.livraria.GerenciadorDeCupons;
 public class ConsultaDeDesconto {
     public static void main(String[] args) {
         GerenciadorDeCupons cupons = new GerenciadorDeCupons();
+        Double desconto = cupons.validaCupom("cab11");
 
-        if(cupons.validaCupom("CUP1234")){
-            System.out.println("Cupom válida");
+        if(desconto != null){
+            System.out.println("Cupom válido");
+            System.out.println("Valor " + desconto);
         }
         else {
             System.out.println("Cupom não existe");
