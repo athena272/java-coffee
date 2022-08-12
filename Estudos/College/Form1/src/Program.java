6d8548735fc9260b8f7ede32de2d4209ae4548b2
athena272 <guilhermera272@gmail.com>
2022-08-12 13:32:45 -0300

    Create Program.java

---
+++ Estudos/College/Form1/src/Program.java
@@ -0,0 +1,43 @@
+public class Program {
+    public static void main(String[] args) {
+//        Questao 1
+//        Fruta fruta = new Fruta("maça");
+//
+//        System.out.println(fruta);
+//        System.out.println(fruta.getEspecie());
+//        ----------------------------------------
+//        Questao 2
+        //Objeto entra no atributo A
+//        Shape shapeAtributo = new Shape();
+//        //Objeto teste
+//        Shape shapeObjeto = new Shape();
+//        shapeObjeto.A = shapeAtributo;
+//        shapeObjeto.B = shapeObjeto.A;
+//
+//        if(shapeObjeto.A.equals(shapeObjeto.B)) {
+//            System.out.println("São Iguais");
+//        }
+//        else {
+//            System.out.println("São Diferentes");
+//        }
+//        System.out.println(shapeObjeto.A);
+//        System.out.println(shapeObjeto.B);
+//        ----------------------------------------------
+//        Questao3
+//    Carro carro = new Carro();
+//    carro.modelo();
+//    ----------------------------------------
+//        Questao4
+//    Forma forma1 = new Forma(4.4);
+//    forma1.setX(7.8);
+//    ----------------------------------------------------
+//    Questao 5
+//    Wii A = new Wii();
+////    São independentes
+//    Wii B = new Wii();
+//    -----------------------------------------------------
+//        Questao 6
+    Forma forma = new Forma(7);
+    System.out.println(forma.getX());
+    }
+}
