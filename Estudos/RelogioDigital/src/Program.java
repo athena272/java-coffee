45eeda8cd278e1390da1ff7f396561a91605870a
athena272 <guilhermera272@gmail.com>
2022-08-13 00:53:40 -0300

    Delete Program.java

--- Estudos/RelogioDigital/src/Program.java
+++
@@ -1,7 +0,0 @@
-public class Program {
-    public static void main(String[] args) {
-        RelogioDigital relogioDigital = new RelogioDigital();
-        relogioDigital.setTempo(19, 12);
-        System.out.println(relogioDigital.getTempo());
-    }
-}
