4ed02db686f419f97431af14d3170cbaaccbc303
athena272 <guilhermera272@gmail.com>
2022-08-13 00:56:12 -0300

    Update Program.java

--- Estudos/College/RelogioDigital/src/Program.java
+++ Estudos/College/RelogioDigital/src/Program.java
@@ -1,7 +1,7 @@
 public class Program {
     public static void main(String[] args) {
         RelogioDigital relogioDigital = new RelogioDigital();
-        relogioDigital.setTempo(19, 12);
+        relogioDigital.horas.;
         System.out.println(relogioDigital.getTempo());
     }
 }
