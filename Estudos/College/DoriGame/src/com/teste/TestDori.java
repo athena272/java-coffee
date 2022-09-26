6a32e41cf184c55c4c8de792caefdaefa8b82564
athena272 <guilhermera272@gmail.com>
2022-09-26 03:56:50 -0300

    Create TestDori.java

---
+++ Estudos/College/DoriGame/src/com/teste/TestDori.java
@@ -0,0 +1,19 @@
+package com.teste;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDori {
+
+  //Constructor
+  public TestDori()
+  {
+
+  }
+
+  @Test 
+  public void testRightDoor() {
+    
+  }
+}
